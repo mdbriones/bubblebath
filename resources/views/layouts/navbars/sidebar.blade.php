8d95472c5a267b0b892d0fdddedfbd3ae036e305
Mark Briones <wp.markbriones@gmail.com>
2019-11-12 16:44:33 +0800

    Initialized new System

---
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -0,0 +1,82 @@
+<div class="sidebar" data-color="orange">
+  <!--
+    Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
+-->
+  <div class="logo" align="center">
+    <a href="{{ route('home') }}" class="simple-text logo-normal">
+      <img src="" alt="">
+      {{ __('BubbleBath') }}
+    </a>
+  </div>
+  <div class="sidebar-wrapper" id="sidebar-wrapper">
+    <ul class="nav">
+      <li class="@if ($activePage == 'home') active @endif">
+        <a href="{{ route('home') }}">
+          <i class="now-ui-icons design_app"></i>
+          <p>{{ __('Dashboard') }}</p>
+        </a>
+      </li>
+      <li>
+        <a data-toggle="collapse" href="#carwashTab">
+            <i class="fa fa-car"></i>
+          <p>
+            {{ __("Carwash") }}
+            <b class="caret"></b>
+          </p>
+        </a>
+        <div class="collapse show" id="carwashTab">
+          <ul class="nav">
+            <li class="@if ($activePage == 'profile') active @endif">
+              <a href="{{ route('profile.edit') }}">
+                <i class="now-ui-icons users_single-02"></i>
+                <p> {{ __("Client Information") }} </p>
+              </a>
+            </li>
+            <li class="@if ($activePage == 'users') active @endif">
+              <a href="{{ route('user.index') }}">
+                <i class="now-ui-icons files_paper"></i>
+                <p> {{ __("Carwash History") }} </p>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li class="@if ($activePage == 'icons') active @endif">
+        <a href="{{ route('page.index','icons') }}">
+          <i class="now-ui-icons education_atom"></i>
+          <p>{{ __('Icons') }}</p>
+        </a>
+      </li>
+      <li class = "@if ($activePage == 'maps') active @endif">
+        <a href="{{ route('page.index','maps') }}">
+          <i class="now-ui-icons location_map-big"></i>
+          <p>{{ __('Maps') }}</p>
+        </a>
+      </li>
+      <li class = " @if ($activePage == 'notifications') active @endif">
+        <a href="{{ route('page.index','notifications') }}">
+          <i class="now-ui-icons ui-1_bell-53"></i>
+          <p>{{ __('Notifications') }}</p>
+        </a>
+      </li>
+      <li class = " @if ($activePage == 'table') active @endif">
+        <a href="{{ route('page.index','table') }}">
+          <i class="now-ui-icons design_bullet-list-67"></i>
+          <p>{{ __('Table List') }}</p>
+        </a>
+      </li>
+      <li class = "@if ($activePage == 'typography') active @endif">
+        <a href="{{ route('page.index','typography') }}">
+          <i class="now-ui-icons text_caps-small"></i>
+          <p>{{ __('Typography') }}</p>
+        </a>
+      </li>
+      <li class = "@if ($activePage == 'upgrade') active @endif active-pro">
+        <a href="{{ route('page.index','upgrade') }}">
+          <i class="now-ui-icons arrows-1_cloud-download-93"></i>
+          <p>{{ __('Upgrade to PRO') }}</p>
+        </a>
+      </li>
+    </ul>
+  </div>
+</div>
